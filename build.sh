72b6589cecc6ab730dedebb783f7ee922156fc8e
dixeran <dixeran@outlook.com>
2022-03-16 00:09:44 +0800

    update build

--- build.sh
+++ build.sh
@@ -14,6 +14,15 @@ export CFLAGS="-I$PWD"
 cp COPYING.LIB ../dist/COPYING.libgnurx
 cp libgnurx-0.dll ../dist/
 cd ../file/
+
+# first, compile natively, to generate a excutable for crosscompile
+make clean
+./configure --prefix=/tmp
+make -j4
+make install
+make clean
+
+# do cross compile
 autoreconf -f -i
 ./configure --disable-silent-rules --enable-fsect-man5
 make -j4
@@ -21,7 +30,8 @@ cp magic/magic.mgc ../dist/
 make clean
 ./configure --disable-silent-rules --enable-fsect-man5 --host=x86_64-w64-mingw32
 # profile FILE_COMPILE since otherwise it would try to use "file.exe" even when cross-compiling
-make -j4 FILE_COMPILE=file
+make -j4 FILE_COMPILE=/tmp/bin/file
 cp src/.libs/libmagic-1.dll ../dist/
 cp src/.libs/file.exe ../dist/
 cp COPYING ../dist/COPYING.file
+
